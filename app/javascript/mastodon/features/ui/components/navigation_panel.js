0342a35b92b50aa78dfbc0f7afeae9c68ce9b211
mohemohe <mohemohe@ghippos.net>
2022-11-14 05:50:20 +0900

    change icon

--- app/javascript/mastodon/features/ui/components/navigation_panel.js
+++ app/javascript/mastodon/features/ui/components/navigation_panel.js
@@ -91,7 +91,7 @@ class NavigationPanel extends React.Component {
             <hr />
 
             <ColumnLink transparent href='/settings/preferences' icon='cog' text={intl.formatMessage(messages.preferences)} />
-            <ColumnLink transparent to='#' icon='cog' text={'plusminus設定 (β)'} onClick={(e) => {
+            <ColumnLink transparent to='#' icon='puzzle-piece' text={'plusminus設定 (β)'} onClick={(e) => {
               e.preventDefault();
               window.__PLUS_MINUS_EVENTS__.dispatchEvent(new Event('openConfig'));
             }}/>
